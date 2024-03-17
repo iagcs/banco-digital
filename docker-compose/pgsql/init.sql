b0661d76ed4f58427ee432f22510745a47c1449f
Iago Souto <iago3220@gmail.com>
2024-03-17 19:11:34 -0300

    Pequeno ajustes

--- docker-compose/pgsql/init.sql
+++ docker-compose/pgsql/init.sql
@@ -1,2 +1,2 @@
 SELECT 'CREATE DATABASE picpay'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'picpay')
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bancodigital')
